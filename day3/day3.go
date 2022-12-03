002fc7d176b271dec2b7e30c8d8178f4c58ba559
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2022-12-03 21:48:19 +0100

    refactored day3

--- day3/day3.go
+++ day3/day3.go
@@ -9,10 +9,8 @@ import (
 func Day3() {
 	fmt.Println("Day 3")
 	lines := utils.GetInput("./day3/input.txt")
-
 	sum1 := task1(lines)
 	fmt.Println("Task1: ", sum1)
-
 	sum2 := task2(lines)
 	fmt.Println("Task2: ", sum2)
 }
@@ -20,10 +18,7 @@ func Day3() {
 func task1(lines []string) int {
 	sum := 0
 	for _, line := range lines {
-		part1, part2 := getParts(line)
-		common := getCommonItem(part1, part2)
-		priority := getPriority(common)
-		sum += priority
+		sum += getPriority(getCommon(getParts(line)))
 	}
 	return sum
 }
@@ -31,48 +26,33 @@ func task1(lines []string) int {
 func task2(lines []string) int {
 	sum := 0
 	for i := 0; i < len(lines); i += 3 {
-		common := getCommonItem3(lines[i], lines[i+1], lines[i+2])
-		sum += getPriority(common)
+		sum += getPriority(getCommon([]string{lines[i], lines[i+1], lines[i+2]}))
 	}
 	return sum
 }
 
-func getParts(line string) (string, string) {
-	part1 := line[:len(line)/2]
-	part2 := line[len(line)/2:]
-	return part1, part2
-}
-
 func getPriority(search string) int {
-	if search == "" {
-		return 0
-	}
-	priorityValues := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	return strings.Index(priorityValues, search) + 1
+	return strings.Index("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", search) + 1
 }
 
-func getCommonItem(part1 string, part2 string) string {
-	for _, i := range part1 {
-		for _, j := range part2 {
-			if i == j {
-				return string(j)
-			}
-		}
-	}
-	return ""
-}
-func getCommonItem3(part1 string, part2 string, part3 string) string {
-	for _, i := range part1 {
-		for _, j := range part2 {
-			if i != j {
-				continue
-			}
-			for _, k := range part3 {
-				if j == k {
-					return string(k)
+func getCommon(parts []string) string {
+	for i := range parts[0] {
+		currentChar := string(parts[0][i])
+		occurance := 1
+		for _, value := range parts[1:] {
+			if strings.Contains(value, currentChar) {
+				occurance++
+				if occurance == len(parts) {
+					return currentChar
 				}
 			}
 		}
 	}
 	return ""
+}
+
+func getParts(line string) []string {
+	part1 := line[:len(line)/2]
+	part2 := line[len(line)/2:]
+	return []string{part1, part2}
 }
